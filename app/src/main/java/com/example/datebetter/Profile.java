df57a2f65185cd962475a241b79eb5460d96a2be
Sarah Ganci <sarahganci@gmail.com>
2019-04-05 12:02:08 -0400

    profile view

---
+++ app/src/main/java/com/example/datebetter/Profile.java
@@ -0,0 +1,24 @@
+package com.example.datebetter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    protected void populate_profile(){
+        //query based on user
+
+        //get and set image
+
+        //get all properties,
+        //loop and create text views for each
+        //add all to about view
+
+    }
+}
